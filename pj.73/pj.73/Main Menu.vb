5da4e437ed7203b33f0fe8c4b9898e634e8b8e8c
Sopitnapa <film0936123963@gmail.com>
2025-08-20 15:11:36 +0700

    เชื่อมหน้าStaff

--- pj.73/pj.73/Main Menu.vb
+++ pj.73/pj.73/Main Menu.vb
@@ -1,3 +1,6 @@
 ﻿Public Class MainMenu
-
+    Private Sub btnStaff_Click(sender As Object, e As EventArgs) Handles btnStaff.Click
+        Dim staffForm As New Staff()
+        staffForm.Show()
+    End Sub
 End Class
